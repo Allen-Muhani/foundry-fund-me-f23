391affdb9940663f8e06c8dedd130c081395b823
allen <allenmuhani@gmail.com>
2025-12-25 19:24:06 +0300

    created a constructore that accepts price feed address, used the deploy script to generate the smart contract address

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,25 +3,27 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe i_fundMe;
 
     function setUp() external {
-        FundMe fundMe = new FundMe();
-        i_fundMe = fundMe;
+
+        DeployFundMe deployer = new DeployFundMe();
+        i_fundMe = deployer.run();
     }
 
-    function testMinimumDollarIsFive() external {
+    function testMinimumDollarIsFive() view external {
         assertEq(i_fundMe.MINIMUM_USD(), 5e18);
     }
 
 
-    function testOwnerIsMsgSender() external {
-        assertEq(i_fundMe.i_owner(), address(this));
+    function testOwnerIsMsgSender() view external {
+        assertEq(i_fundMe.i_owner(), msg.sender);
     }
 
-    function testPriceFeedVersionIsAccurate() external {
+    function testPriceFeedVersionIsAccurate() view external {
         uint256 version = i_fundMe.getVersion();
         assertEq(version, 4);
     }
